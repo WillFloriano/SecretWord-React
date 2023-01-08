ebcc6c3a709914edf6baa2d6c5c6d0b8a7036086
Will <william.ffloriano@gmail.com>
2023-01-08 02:31:21 -0300

    ajuste word

--- src/data/words.js
+++ src/data/words.js
@@ -1,6 +1,6 @@
 export const wordsList = {
     carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Freio", "Retrovisor", "Farol","Câmbio","Seta","Embreagem"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Limão", "Pessego", "Ameixa", "Abacaxi","Uva Verde", "Abacate", "Manga"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Limão", "Pessego", "Ameixa", "Abacaxi","Uva", "Abacate", "Manga"],
     corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Joelho", "Boca", "Cotovelo"],
     computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Web Cam", "Placa Mâe", "Usb", ""],
     programação: ["Linguagem", "Framework", "JavaScript", "React", "Html"],
